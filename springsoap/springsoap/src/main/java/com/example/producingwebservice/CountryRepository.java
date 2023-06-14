dcd6efab806b4787bc93a89b33093b435c572620
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-06-14 10:40:30 +0300

    Add files via upload

---
+++ springsoap/springsoap/src/main/java/com/example/producingwebservice/CountryRepository.java
@@ -0,0 +1,50 @@
+package com.example.producingwebservice;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import io.spring.guides.gs_producing_web_service.Country;
+
+@Component
+public class CountryRepository {
+    private static final Map<String, Country> countries = new HashMap<>();
+
+    /**
+     * 
+     */
+    @PostConstruct
+    static final void initData() {
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+
+        spain.setPopulation(46704314);
+
+        countries.put(spain.getName(), spain);
+
+        final Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+
+        poland.setPopulation(38186860);
+
+        countries.put(poland.getName(), poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setPopulation(63705000);
+
+        countries.put(uk.getName(), uk);
+    }
+
+    public Country findCountry(String name) {
+        Assert.notNull(name, "The country's name must not be null");
+        return countries.get(name);
+    }
+}
---
+++ springsoap/springsoap/src/test/java/com/example/springsoap/SpringsoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
